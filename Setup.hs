edb452757adae09f3f42a7d021effeab7a87a1fa
Andreas Abel <andreas.abel@ifi.lmu.de>
2025-01-14 19:05:55 +0100

    Re #1: make `cabal repl` work

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,6 @@
 -- | Hook BNFC into the cabal build process to generate AST, lexer, parser, and printer definitions.
 
-import Distribution.Simple            (defaultMainWithHooks, simpleUserHooks, buildHook)
+import Distribution.Simple            (defaultMainWithHooks, simpleUserHooks, buildHook, replHook)
 import Distribution.Simple.BuildPaths (autogenPackageModulesDir)
 import System.Process                 (callProcess)
 
@@ -16,4 +16,14 @@ main = defaultMainWithHooks simpleUserHooks
         ]
       -- Run the build process.
       buildHook simpleUserHooks packageDescription localBuildInfo userHooks buildFlags
+  , replHook = \ packageDescription localBuildInfo userHooks buildFlags replArgs-> do
+      -- For simplicity, generate files in build/global-autogen;
+      -- there they are available to all components of the package.
+      callProcess "bnfc"
+        [ "-o", autogenPackageModulesDir localBuildInfo
+        , "-d"
+        , "src/LBNF.cf"
+        ]
+      -- Run the build process.
+      replHook simpleUserHooks packageDescription localBuildInfo userHooks buildFlags replArgs
   }
